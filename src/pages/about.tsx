aeaaa2994700197f819b636680a21c6a16011e5d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-21 22:12:01 +0900

    新規ページの作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+const about = () => {
+  return (
+    <div>
+      Cattery
+      CANONは、神奈川県座間市にある、メインクーン専門の小さなキャテリーです。アメリカに本部を置く愛猫団体
+      CFA TICAに所属しています。
+    </div>
+  );
+};
+
+export default about;
---
+++ src/pages/my-cats.tsx
@@ -0,0 +1,10 @@
+const myCats = () => {
+  return (
+    <div className=''>
+      <div>boys</div>
+      <div>girls</div>
+    </div>
+  );
+};
+
+export default myCats;
