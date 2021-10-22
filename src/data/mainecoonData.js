e44a8f755c90aa5b7c69b8be60978374592cd236
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-22 10:58:08 +0900

    データを修正

---
+++ src/data/mainecoonData.js
@@ -0,0 +1,41 @@
+export const mainecoonDataBoy = [
+  {
+    title: "Boy1",
+    description1: `AAA`,
+    description2: `今まで見つけることができなかった数百もの微小ピークの探索に適しています。`,
+    imgSrc: "/static/images/header/baby-on-piano.jpg",
+  },
+  {
+    title: "Boy2",
+    description1: `BBB`,
+    description2: `液体クロマトグラフ-質量分析計、キャピラリー電気泳動-質量分析計のデータ解析も可能です。`,
+    imgSrc: "/static/images/header/brown-baby-02.jpg",
+  },
+  {
+    title: "Boy3",
+    description1: `CCC`,
+    description2: `共通ソフトウェアで解析する場合に便利です。`,
+    imgSrc: "/static/images/header/baby-on-piano.jpg",
+  },
+  {
+    title: "Boy4",
+    description1: `DDD`,
+    description2: `生理活性物質 農薬 生物 ・ 環境中物質 薬物など各分野のライブラリを取り揃えております。`,
+    imgSrc: "/static/images/header/red-kitten.jpg",
+  },
+];
+
+export const mainecoonDataGirl = [
+  {
+    title: "Girl1",
+    description1: `EEE`,
+    description2: `においの閾（いき）値などを予測します。muth博士により監修されたライブラリ付ソフトウェアです。`,
+    imgSrc: "/static/images/header/baby-on-piano.jpg",
+  },
+  {
+    title: "Girl2",
+    description1: `FFF`,
+    description2: `今まで見つけることができなかった数百もの微小ピークの探索に適しています。`,
+    imgSrc: "/static/images/header/baby-on-piano.jpg",
+  },
+];
