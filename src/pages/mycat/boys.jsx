ee5d3f873c803f76655c85d4d7a32380b5ffeeca
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-22 12:31:49 +0900

    boys/girlsページ実装

---
+++ src/pages/mycat/boys.jsx
@@ -0,0 +1,85 @@
+/* eslint-disable react/jsx-handler-names */
+/* eslint-disable import/no-default-export */
+import { useCallback, useState } from "react";
+import { useModal } from "react-hooks-use-modal";
+import { Card } from "src/components/Card";
+import { CardModal } from "src/components/Card";
+import { PageTitle } from "src/components/PageTitle";
+import { PageSEO } from "src/components/SEO";
+import { mainecoonDataBoy } from "src/data/mainecoonData";
+import { siteMetadata } from "src/data/siteMetadata";
+import { Layout } from "src/layout";
+
+const MyBoys = () => {
+  const [Modal, open, close] = useModal("root", {
+    preventScroll: true,
+  });
+  const [selectedItem, setSelectedItem] = useState();
+
+  const handleOnClick = useCallback(
+    (d) => {
+      const result = mainecoonDataBoy.find(({ title }) => {
+        return title === d.title;
+      });
+      // console.log(result);
+      setSelectedItem(result);
+      open();
+    },
+    [open]
+  );
+
+  return (
+    <Layout theme="main">
+      <PageSEO title={`MyCats- ${siteMetadata.author}`} description={siteMetadata.description1} />
+      <PageTitle>Boys- Cattery Canonの男の子たち</PageTitle>
+      <div className="divide-y divide-gray-200 ">
+        <div className="container py-12">
+          <div className="flex flex-wrap m-4">
+            {mainecoonDataBoy.map((d) => {
+              // console.log(d);
+              return (
+                <button
+                  onClick={() => {
+                    return handleOnClick(d);
+                  }}
+                  description={d.description1}
+                  description2={d.description2}
+                  key={d.title}
+                  // href={d.href}
+                  className="md:w-1/2 md"
+                >
+                  <Card key={d.href} title={d.title} description={d.description1} imgSrc={d.imgSrc} />
+                </button>
+              );
+            })}
+          </div>
+
+          <Modal>
+            <div className="bg-white px-2 sm:px-4 md:px-10 py-10 rounded text-gray-500">
+              <h1 className="mt-5">Boys - Cattery Canonの男の子たち</h1>
+              <p>
+                <CardModal
+                  title={selectedItem?.title}
+                  description1={selectedItem?.description1}
+                  description2={selectedItem?.description2}
+                  imgSrc={selectedItem?.imgSrc}
+                  // href={selectedItem?.href}
+                />
+              </p>
+              <div className="flex justify-end mt-8">
+                <button
+                  onClick={close}
+                  className="rounded p-1 text-center font-medium text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 focus:from-purple-600 focus:to-yellow-600 opacity-80 mb-3 lg:mr-3 lg:py-2 hover:text-gray-100"
+                >
+                  閉じる
+                </button>
+              </div>
+            </div>
+          </Modal>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default MyBoys;
