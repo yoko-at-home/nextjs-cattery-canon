01171cfb6b8e099ed894ce93d3eb552d9cad0c20
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-22 13:04:16 +0900

    SEO対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { PageSEO } from "src/components/SEO";
 import { TopPage } from "../components/TopPage";
+import { siteMetadata } from "../data/siteMetadata";
 
 const Home = () => {
-  return <TopPage />;
+  return (
+    <>
+      <PageSEO
+        title={`Top - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
+      <TopPage />
+    </>
+  );
 };
 
 export default Home;
