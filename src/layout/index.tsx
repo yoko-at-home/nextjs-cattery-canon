3a006d8a446cfe804943be61bdc734738243d42a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-22 13:31:45 +0900

    💄 desktop navに背景色を追加

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -13,15 +13,13 @@ type Props = {
 
 export const Layout: VFC<Props> = (props) => {
   return (
-    <div className='flex'>
-      <div className="hidden sm:block w-2/6 lg:w-1/6">
+    <div className="flex justify-between">
+      <div className="hidden sm:block w-2/6 lg:w-1/6 bg-gradient-to-br from-yellow-900 text-yellow-700 min-h-full rounded-full m-5">
         <NavBarDesktop />
       </div>
-      <div className='w-screen md:w-5/6 lg:w-4/6'>
+      <div className="w-screen md:w-5/6 lg:w-4/6">
         <Header theme={props.theme || "main"} />
-        <main className='mx-3 sm:mx-10 md:mx-auto md:w-11/12 lg:w-9/12'>
-          {props.children}
-        </main>
+        <main className="mx-3 sm:mx-10 md:mx-auto md:w-11/12 lg:w-9/12">{props.children}</main>
         <Footer />
       </div>
       <div className='hidden lg:block lg:w-1/6'>vertical Carousel</div>
