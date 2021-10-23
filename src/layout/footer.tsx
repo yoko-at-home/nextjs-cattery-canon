dd51f3aa4c5ad1ac36aa1c280081e9499d376051
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-23 09:16:37 +0900

    footerの更新

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -7,9 +7,9 @@ import { CustomLink } from "../components/CustomLink";
 export const Footer: VFC = () => {
   return (
     <footer>
-      <div className='flex flex-col items-center mt-16'>
+      <div className='flex flex-col items-center mt-16 justify-center whitespace-nowrap'>
         <div className='flex mb-2 space-x-2 text-sm text-gray-500 '>
-          <div>All rights reserved by: {siteMetadata.author}</div>
+          <div>All rights reserved by {siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
@@ -17,12 +17,12 @@ export const Footer: VFC = () => {
             target='_blank'
             rel=' noopener'
             href='http://catterycanoncat.blog.fc2.com'
-            className='hover:text-green-600 hover:transform skew-y-12'
+            className='animate-pulse text-gray-700 hover:text-green-600'
           >
             {siteMetadata.title} ブログ
           </a>
         </div>
-        <div className='my-8 text-sm text-gray-500'>
+        <div className='my-8 text-sm text-gray-500 bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-blue-700'>
           <CustomLink href='https://yoko-2021.vercel.app/'>
             Powered by yoko
           </CustomLink>
