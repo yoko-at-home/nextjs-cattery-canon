e170bd8f3ee18f78d030a81115eb571ac2061b33
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-23 10:12:35 +0900

    ダミー文書更新

--- src/data/mainecoonData.js
+++ src/data/mainecoonData.js
@@ -2,25 +2,25 @@ export const mainecoonDataBoy = [
   {
     title: "Boy1",
     description1: `AAA`,
-    description2: `今まで見つけることができなかった数百もの微小ピークの探索に適しています。`,
+    description2: `Rem provident praesentium id labore `,
     imgSrc: "/static/images/header/baby-on-piano.jpg",
   },
   {
     title: "Boy2",
     description1: `BBB`,
-    description2: `液体クロマトグラフ-質量分析計、キャピラリー電気泳動-質量分析計のデータ解析も可能です。`,
+    description2: `reprehenderit repellendus fugiat laboriosam itaque ratione a assumenda dolores molestias distinctio, voluptate impedit esse cumque expedita aliquid!`,
     imgSrc: "/static/images/header/brown-baby-02.jpg",
   },
   {
     title: "Boy3",
     description1: `CCC`,
-    description2: `共通ソフトウェアで解析する場合に便利です。`,
+    description2: `Rem provident praesentium id labore reprehenderit repellendus fugiat laboriosam itaque ratione a assumenda dolores molestias distinctio, voluptate impedit esse cumque expedita aliquid!`,
     imgSrc: "/static/images/header/baby-on-piano.jpg",
   },
   {
     title: "Boy4",
     description1: `DDD`,
-    description2: `生理活性物質 農薬 生物 ・ 環境中物質 薬物など各分野のライブラリを取り揃えております。`,
+    description2: `Rem provident praesentium id labore reprehenderit repellendus fugiat laboriosam itaque ratione a assumenda dolores molestias distinctio, voluptate impedit esse cumque expedita aliquid!`,
     imgSrc: "/static/images/header/red-kitten.jpg",
   },
 ];
@@ -29,13 +29,13 @@ export const mainecoonDataGirl = [
   {
     title: "Girl1",
     description1: `EEE`,
-    description2: `においの閾（いき）値などを予測します。muth博士により監修されたライブラリ付ソフトウェアです。`,
+    description2: `Rem provident praesentium id labore reprehenderit repellendus fugiat laboriosam itaque ratione a assumenda dolores molestias distinctio`,
     imgSrc: "/static/images/header/baby-on-piano.jpg",
   },
   {
     title: "Girl2",
     description1: `FFF`,
-    description2: `今まで見つけることができなかった数百もの微小ピークの探索に適しています。`,
+    description2: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. `,
     imgSrc: "/static/images/header/baby-on-piano.jpg",
   },
 ];
