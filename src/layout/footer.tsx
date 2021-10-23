0fb721e5af5c55507d3a78fc4eb6dc488faa551a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-23 11:12:23 +0900

    メールアイコンを追加

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -12,6 +12,23 @@ export const Footer: VFC = () => {
           <div>All rights reserved by {siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
+        </div>
+        <div className='flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13'>
+          <a
+            href='mailto:`${siteMetadata.email}`'
+            title='Email'
+            className='flex items-center justify-center w-8 h-8 rounded-full bg-yellow-700 text-gray-200'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              className='w-5 h-5'
+            >
+              <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'></path>
+              <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'></path>
+            </svg>
+          </a>
           <div>{` • `}</div>
           <a
             target='_blank'
