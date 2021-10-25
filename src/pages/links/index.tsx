7e49d380ad4a5ac05e1bedc05d7fcee505a0b87e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-25 09:21:01 +0900

    linksの画像取得元をローカル画像に変更

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -10,31 +10,29 @@ const links = [
     description:
       "Canonのブログです。Canonの子供たちの様子や最新情報をお知らせしています。",
     url: "http://catterycanoncat.blog.fc2.com",
-    backgroundImg: 'url("https://source.unsplash.com/random/240x320?cats")',
+    backgroundImg:
+      'url("https://source.unsplash.com/random/240x320?mainecoon")',
   },
   {
     title: "湘南キャットクラブ",
     description:
       "Canonが所属しております。ショーナンキャットクラブは、アメリカテキサス州に本部を置くTICA(The International Cat Association)公認ブランチです。",
     url: "http://www.shonan-catclub.com",
-    backgroundImg:
-      'url("https://source.unsplash.com/random/241x320?mainecoon")',
+    backgroundImg: 'url("/static/images/links/SCC.jpg")',
   },
   {
     title: "The Cat Fanciers’ Association",
     description:
       "アメリカオハイオ州に本部を置く、The Cat Fanciers’ AssociationのWebサイトです。Mainecoonのホットな情報が掲載されます。",
     url: "https://cfa.org/maine-coon-cat/",
-    backgroundImg:
-      'url("https://source.unsplash.com/random/242x320?ginger?tabby?")',
+    backgroundImg: 'url("/static/images/links/TCFA.jpg")',
   },
   {
     title: "The International Cat Association (TICA)",
     description:
       "血統書付き猫と家庭猫の世界最大の登録機関。Canonは正規会員です。",
     url: "https://tica.org/ja/",
-    backgroundImg:
-      'url("https://source.unsplash.com/random/243x320?kitten?nature?")',
+    backgroundImg: 'url("/static/images/links/TICA.jpg")',
   },
 ];
 
@@ -65,7 +63,7 @@ const Links: NextPage = () => {
                         backgroundImage: link.backgroundImg,
                       }}
                     >
-                      <div className='rounded bg-black bg-opacity-30 absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-coolGray-900'></div>
+                      <div className='rounded bg-black bg-opacity-60 absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-coolGray-900'></div>
                       <div className='absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3'>
                         {link.title}
                         <div className='flex flex-col justify-start text-center'>
