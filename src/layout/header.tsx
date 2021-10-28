69377be5cb80c4a8769b55a38f9fb4461a7b7587
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-28 13:36:58 +0900

    画像の入れ替え

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -11,10 +11,10 @@ type Props = {
 
 export const Header: VFC<Props> = (props) => {
   const themeImage = {
-    home: "/static/images/header/brown-baby-02.jpg",
+    home: "/static/images/header/four-kittens.jpg",
     boys: "/static/images/header/five-kids.jpg",
-    girls: "/static/images/header/four-kittens.jpg",
-    retired: "/static/images/header/maincoon-on-white-background.jpg",
+    girls: "/static/images/header/maincoon-on-white-background.jpg",
+    retired: "/static/images/header/brown-baby-02.jpg",
     available: "/static/images/header/red-kitten.jpg",
     articles: "/static/images/header/three-kids.jpg",
   };
@@ -22,23 +22,26 @@ export const Header: VFC<Props> = (props) => {
   return (
     <>
       <header>
-        <div className='relative rounded nm-flat-gray-100-xl h-full'>
+        <div className="relative rounded nm-flat-gray-100-xl h-full">
           <img
-            width='100%'
-            height='90%'
-            className='relative rounded object-cover object-center pointer-events-none'
+            width="100%"
+            height="90%"
+            className="relative rounded object-cover object-center pointer-events-none"
             src={themeImage[props.theme || "home"]}
             alt={"Canon's mainecoons"}
           />
-          <h1 className='absolute top-10 p-5 sm:left-10 md:top-14 lg:top-10'>
-            <CustomLink href='/home' aria-label='Gen-Scent Research Laboratory'>
-              <div className='flex items-center justify-between mb-8'>
+          <h1 className="absolute -bottom-5 p-5 sm:left-10 md:-bottom-10">
+            <CustomLink href="/home" aria-label="Gen-Scent Research Laboratory">
+              <div className="flex items-center justify-between mb-8">
                 {typeof siteMetadata.headerTitle === "string" ? (
                   <div
-                    className='min-h-full min-w-full py-5 pr-20 text-green-900 text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold my-6 md:mb-20 md:mt-12 whitespace-nowrap'
                     style={{
-                      textShadow: "5px 5px 6px rgb(99, 51, 7, .6)",
+                      border: "double 5px #636363",
+                      textShadow: "3px 3px 5px#ffffff",
+                      color: "#71745b",
+                      // background: "linearGradient(75deg;rgba(2, 9, 66, 1) 4 % rgba(6, 85, 92, 1) 76%)",
                     }}
+                    className="bg-opacity-40 backdrop-filter backdrop-blur-lg py-1 pr-3 sm:py-3 sm:pr-4 bg-clip-text text-2xl sm:text-5xl xl:text-6xl font-semibold my-6 md:mb-20 md:mt-12 whitespace-nowrap"
                   >
                     {siteMetadata.headerTitle}
                   </div>
@@ -49,7 +52,7 @@ export const Header: VFC<Props> = (props) => {
             </CustomLink>
           </h1>
         </div>
-        {props.theme !== "articles" && <NavBarMobile type='main' />}
+        {props.theme !== "articles" && <NavBarMobile type="main" />}
       </header>
     </>
   );
