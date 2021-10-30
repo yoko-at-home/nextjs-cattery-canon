9ba9943d6b5794e021620900bac7e7180856a423
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-30 21:29:09 +0900

    ✨ GoogleAnalyticsの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
+import { GoogleAnalytics } from "src/components/GoogleAnalytics";
 
 import type { AppProps } from "next/app";
+
+<GoogleAnalytics />;
 
 const App = (props: AppProps) => {
   return <props.Component {...props.pageProps} />;
