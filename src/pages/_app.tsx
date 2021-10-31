c78660c39a12f5d4c5b3b5a548e6b3b0ef3709d0
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-31 15:15:44 +0900

    Analyticsの呼び出し位置を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import { GoogleAnalytics } from "src/components/GoogleAnalytics";
-
 import type { AppProps } from "next/app";
 
-<GoogleAnalytics />;
-
 const App = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />;
+      <props.Component {...props.pageProps} />
+    </>
+  );
 };
 
 export default App;
