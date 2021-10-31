2358c46b7137c4d48aea55e7b4fca6c82509c990
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-31 16:35:47 +0900

    RouterのURL書き換えが完了した時に発火するrouteChangeCompleteイベント実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import { GoogleAnalytics } from "src/components/GoogleAnalytics";
 import type { AppProps } from "next/app";
+import usePageView from 'src/hooks/usePageView';
 
 const App = (props: AppProps) => {
+  usePageView();
   return (
     <>
       <GoogleAnalytics />
