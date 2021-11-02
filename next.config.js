4dcf28ae8b4df7d6833c8e3a362beca68b70eb61
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-02 11:43:42 +0900

    ✨ next12をインストール

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+const nextConfig = {
+  rewrites: async () => {
+    return [{ source: "/", destination: "/root" }];
+  },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
+  future: { strictPostcssConfiguration: true },
+  pageExtensions: [".tsx", ".ts", ".jsx", ".js"],
   reactStrictMode: true,
+  poweredByHeader: false,
+};
+
+module.exports = {
+  nextConfig,
   images: {
     domains: [
       "images.unsplash.com",
