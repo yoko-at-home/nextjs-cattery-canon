3decb9fe2767eb64b36bc6b713ce61ebf472ddbc
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-02 17:23:04 +0900

    🚨 fix lint error

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,6 +1,6 @@
-import { VFC } from "react";
-import { dataVertical } from "src/data/carouselVerticalData";
 import Image from "next/image";
+import type { VFC } from "react";
+import { dataVertical } from "src/data/carouselVerticalData";
 
 type Props = {
   title?: string;
@@ -9,10 +9,16 @@ type Props = {
 
 export const Tile: VFC<Props> = (props) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-items-stretch gap-0">
+    <div className="grid grid-cols-1 gap-0 justify-items-stretch sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
       {dataVertical.map((data) => {
         return (
-            <Image src={data.src} alt={data.title} width={400} height={350} className="object-cover opacity-80 hover:opacity-100" />
+          <Image
+            src={data.src}
+            alt={data.title}
+            width={400}
+            height={350}
+            className="object-cover opacity-80 hover:opacity-100"
+          />
         );
       })}
     </div>
