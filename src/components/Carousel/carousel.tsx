48cedf287646720f063b04feea760841cf0a7c3b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-09 21:27:04 +0900

    topページをbackgroundimageからImageに変更

---
+++ src/components/Carousel/carousel.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import Image from "next/image";
+
+export const CarouselTop = () => {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout: ReturnType<typeof setTimeout>;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
+
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">
+          <Image layout="fill" src="/static/images/0_top/01_mainecoon-girls.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide2">
+          <Image layout="fill" src="/static/images/0_top/02_baby-mainecoon.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide3">
+          <Image layout="fill" src="/static/images/0_top/03_in-a-field.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/04_kenta.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/05_mainecoon-baby.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/06_mainecoon.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/07_painting-green.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/08_painting.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/09_red-plate.webp" />
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <Image layout="fill" src="/static/images/0_top/10_sleeping-baby.webp" />
+        </div>
+      </div>
+      <style jsx>{`
+        [class^="number-slide"],
+        [class*=" number-slide"] {
+          // background: grey;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 50px;
+          color: #fff;
+          font-weight: 500;
+          height: 100vh;
+          max-height: 100vh;
+          width: 100vw;
+        }
+      `}</style>
+    </>
+  );
+};
--- src/layout/LayoutCarousel.tsx
+++ src/layout/LayoutCarousel.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode, VFC } from "react";
-import { Carousel } from "src/components/Carousel";
+import { CarouselTop } from "src/components/Carousel";
 import { NavBarMobile } from "src/components/navbar";
 
 type Props = {
@@ -8,8 +8,8 @@ type Props = {
 
 export const LayoutCarousel: VFC<Props> = (props) => {
   return (
-    <div className="relative">
-      <Carousel />
+    <div className="relative w-screen">
+      <CarouselTop />
       <main className="absolute top-20 md:top-40">{props.children}</main>
       <NavBarMobile type="entrance" />
     </div>
