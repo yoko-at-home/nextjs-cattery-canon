ed9cdd13781cbfef1bc21c65e2071295522b5221
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-12-27 11:02:27 +0900

    tsx化に対応

--- src/components/Carousel/test.tsx
+++ src/components/Carousel/test.tsx
@@ -1,7 +1,8 @@
+import { NextPage } from "next";
 import Image from "next/image";
 import { dataVertical } from "src/data/carouselVerticalData";
 
-export const Test = () => {
+export const Test:NextPage = () => {
   return (
     <div className="flex overflow-y-hidden flex-col items-center">
       {dataVertical.map((item) => {
