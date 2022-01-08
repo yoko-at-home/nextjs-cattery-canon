d3a44c480e515154657d8d7309f663ab5c99c9d8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-08 19:35:25 +0900

    babyページの作成

---
+++ src/pages/baby/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { PageTitle } from "src/components/PageTitle";
+import { PageSEO } from "src/components/SEO";
+import { siteMetadata } from "src/data/siteMetadata";
+import { Layout } from "src/layout";
+
+const Baby = () => {
+  return (
+    <div className="bg-[#35a4ac] relative">
+      <Layout theme="home">
+        <PageSEO title={`赤ちゃん - ${siteMetadata.author}`} description={siteMetadata.description} />
+
+        <PageTitle type="large">
+          <span className="text-white">かわいい赤ちゃんが誕生しました ...こちらでお知らせできるまでしばらくお待ちください</span>
+        </PageTitle>
+        <div className="orb"></div>
+        <style jsx>
+          {`
+            .orb {
+              animation: spin 4s linear infinite;
+              width: 250px;
+              height: 250px;
+              margin: auto;
+              margin-top: 100px;
+              border-radius: 50%;
+              box-shadow: inset 0 0 50px #fff, inset 20px 0 60px violet, inset -20px 0 60px #0ff,
+                inset 20px 0 250px violet, inset -20px 0 250px #0ff, 0 0 50px #fff, -10px 0 60px violet,
+                10px 0 60px #0ff;
+            }
+            @keyframes spin {
+              100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg);
+              }
+            }
+          `}
+        </style>
+      </Layout>
+    </div>
+  );
+};
+
+export default Baby;
