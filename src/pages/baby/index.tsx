fc52e5725669f6ebf7c0c9403d867f00f66f443f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-16 16:27:22 +0900

    Baby: 動画を追加

--- src/pages/baby/index.tsx
+++ src/pages/baby/index.tsx
@@ -4,10 +4,9 @@ import { PageSEO } from "src/components/SEO";
 import { siteMetadata } from "src/data/siteMetadata";
 import { Layout } from "src/layout";
 
-const description =
-  "クリスマスイブのイブにかわいい赤ちゃんが誕生しました ...こちらでお知らせできるまでしばらくお待ちください";
+const description = "クリスマスイブのイブにかわいい赤ちゃんが誕生しました";
 
-const Baby:NextPage = () => {
+const Baby: NextPage = () => {
   return (
     <div className="relative bg-[#50c4cc]">
       <Layout theme="baby" photographer="Canon">
@@ -16,15 +15,25 @@ const Baby:NextPage = () => {
         <PageTitle type="medium">
           <span className="text-white">{description}</span>
         </PageTitle>
-        <div className="orb"></div>
+        <div className="flex flex-col items-center">
+          <div className="orb" />
+          <div className="flex flex-col justify-evenly sm:flex-row items-center">
+            <div className="orb" />
+            <div className="orb2 flex m-6 justify-items-end h-96 w-80 rounded-full overflow-hidden border-double border-4 border-pink-200">
+              <div className="orb2">
+                <video autoPlay muted loop src="/static/gif/2022-babies.mov" />
+              </div>
+            </div>
+            <div className="orb" />
+          </div>
+          <div className="orb" />
+        </div>
         <style jsx>
           {`
             .orb {
               animation: spin 4s linear infinite;
-              width: 250px;
-              height: 250px;
-              margin: auto;
-              margin-top: 100px;
+              width: 100px;
+              height: 100px;
               border-radius: 50%;
               box-shadow: inset 0 0 50px #fff, inset 20px 0 60px violet, inset -20px 0 60px #0ff,
                 inset 20px 0 250px violet, inset -20px 0 250px #0ff, 0 0 50px #fff, -10px 0 60px violet,
@@ -35,6 +44,12 @@ const Baby:NextPage = () => {
                 -webkit-transform: rotate(360deg);
                 transform: rotate(360deg);
               }
+            }
+            .orb2 {
+              box-shadow: inset 0 0 150px #fff, inset 20px 0 160px violet, inset -20px 0 160px #0ff,
+                inset 20px 0 250px violet, inset -20px 0 250px #0ff, 0 0 50px #fff, -10px 0 160px violet,
+                10px 0 60px #0ff;
+            }
             }
           `}
         </style>
