dfb4c99266e19f49438fb13aa95c3228c5deaa84
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-25 10:49:31 +0900

    fix obsolute tailwind css property

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -8,9 +8,14 @@ import { PageSEO } from "./SEO";
 export const TopPage = () => {
   return (
     <LayoutCarousel>
-      <PageSEO title={`ようこそ！- ${siteMetadata.author}`} description={siteMetadata.description} />
-
-      <div className="py-12 text-xs leading-7 text-center text-white uppercase bg-black rounded-full sm:text-xl bg-opacity-30">
+      <PageSEO
+        title={`ようこそ！ - ${siteMetadata.title}`}
+        description={siteMetadata.description}
+        ogType="website"
+        ogImage={siteMetadata.siteUrl + siteMetadata.ogImage}
+        siteUrl={siteMetadata.siteUrl}
+      />{" "}
+      <div className="py-12 text-xs leading-7 text-center text-white uppercase bg-black/30 rounded-full sm:text-xl">
         <div className="p-6">
           <h1 className="text-3xl animation" style={{ textShadow: "0px 10px 3px rgba(0, 0, 0, 0.9)" }}>
             Welcome to
@@ -24,7 +29,7 @@ export const TopPage = () => {
           Fanciers Association (CFA) and The International Cat Association (TICA). The headquarters are in USA.
         </div>
         <Link href="/home">
-          <a className="inline-block p-4 text-2xl hover:text-yellow-600 bg-yellow-400 hover:bg-green-700 bg-gradient-to-tl from-pink-800 rounded md:text-4xl bg-opacity-80 animation leading-looses nm-inset-yellow-700-xs">
+          <a className="inline-block p-4 text-2xl hover:text-yellow-600/80 bg-yellow-400/80 hover:bg-green-700/80 bg-gradient-to-tl from-pink-800/80 rounded md:text-4xl animation leading-looses nm-inset-yellow-700-xs">
             Enter
           </a>
         </Link>
