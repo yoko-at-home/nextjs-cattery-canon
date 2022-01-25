561421386104baf374b985b27b097389c9dfd2ff
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-01-25 14:44:49 +0900

    Google インデックス 登録のリクエスト処理

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,10 @@ const MyDocument = () => {
         <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" crossOrigin="use-credentials" />
         <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#cf980a" />
+        {/* インデックス 登録のリクエスト処理 */}
+        <meta name="google-site-verification" content="b8bHgMnOcmFYxe-Nz8dhHx0AKW5h8BgWvAWKWreYUbQ" />
         <meta name="msapplication-TileColor" content="#cf980a" />
         <meta name="theme-color" content="#f7ecd2" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
