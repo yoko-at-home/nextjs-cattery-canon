542d6c6ac8c25f5047d9ea2d3c3a967f88da6426
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-27 13:04:38 +0900

    fix

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -19,7 +19,7 @@ export const PageTitle: FC<Props> = (props) => {
       className={cc([
         "bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-green-900 font-extrabold leading-loosee tracking-loose py-10",
         {
-          "text-4xl md:text-5xl tracking-widest": props.type === "x-large",
+          "text-5xl tracking-widest": props.type === "x-large",
           "text-4xl": props.type === "large",
           "text-3xl": props.type === "medium",
           "text-lg": props.type === "small",
