229cc3b695bc4196fee52e602a9235d5ad1cfb63
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-01-06 20:39:35 +0900

    set microcms domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   poweredByHeader: false,
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
 };
 
 module.exports = nextConfig;
