5d5278cbba388ec1d09c3ac9773963c8d38ebfdb
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-06-05 21:58:16 +0900

    upgrade tailwind version

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-nesting": {},
+    "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     autoprefixer: {},
   },
